f5c3f68a270b7ece9de9a7d19ef078fadcbdb8d3
alcinavanhorn <alcinavanhorn@gmail.com>
2018-10-26 12:14:37 +0300

    Adding REST services

--- src/main/java/fi/haagahelia/course/domain/Book.java
+++ src/main/java/fi/haagahelia/course/domain/Book.java
@@ -6,6 +6,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class Book {
 	@Id
@@ -18,6 +20,7 @@ public class Book {
     private double price;
     
     @ManyToOne
+    @JsonIgnore
     @JoinColumn(name = "categoryid")
     private Category category;
     
